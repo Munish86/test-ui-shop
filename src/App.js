f2772923a496e67cb052d940fece8ced12555cc7
munish <munishk4486@gmail.com>
2025-06-25 23:48:49 +0530

    home-change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is homepage
+          This is homepage beautiful
         </p>
         <a
           className="App-link"
